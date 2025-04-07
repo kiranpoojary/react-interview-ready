41d64e661d713861817c1be8a9b6ae9170509155
Kiran Poojary <kiranpoojary483@gmail.com>
2025-04-07 12:19:37 +0530

    cleanup

--- src/TopQuestions/OptionSelector.jsx
+++ src/TopQuestions/OptionSelector.jsx
@@ -89,7 +89,6 @@ export default function OptionSelector() {
         checkedHistory={checkedHistory}
         setCheckedHistory={setCheckedHistory}
       />
-      {JSON.stringify(checkedHistory)}
     </div>
   );
 }
